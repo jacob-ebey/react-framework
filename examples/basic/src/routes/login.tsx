178550369d8e2147d5cfc085f2f672f501aebd56
Jacob Ebey <jacob.ebey@live.com>
2024-09-25 15:39:45 -0700

    cleanup example

--- examples/basic/src/routes/login.tsx
+++ examples/basic/src/routes/login.tsx
@@ -16,25 +16,6 @@ export function eyeball() {
 	}
 }
 
-// A form action to log in a user.
-async function loginAction(formData: FormData) {
-	"use server";
-
-	// Get the current request context.
-	const c = getContext();
-	const input = await validateLoginInput(formData);
-	if (!input.valid) return "Invalid email or password.";
-
-	// Validate the user's credentials.
-	const db = c.env.DB.get(c.env.DB.idFromName(""));
-	const user = await db.loginUser(input.data);
-	if (!user) return "Invalid email or password.";
-
-	// Set the user's ID in a signed cookie.
-	c.cookie.set("userId", user.id);
-	throw c.redirect("/profile");
-}
-
 export default function LoginRoute() {
 	// Get the state of the login action.
 	const login = getAction(loginAction);
@@ -56,4 +37,23 @@ export default function LoginRoute() {
 			{!!login.data && <p>{login.data}</p>}
 		</form>
 	);
+}
+
+// A form action to log in a user.
+async function loginAction(formData: FormData) {
+	"use server";
+
+	// Get the current request context.
+	const c = getContext();
+	const input = await validateLoginInput(formData);
+	if (!input.valid) return "Invalid email or password.";
+
+	// Validate the user's credentials.
+	const db = c.env.DB.get(c.env.DB.idFromName(""));
+	const user = await db.loginUser(input.data);
+	if (!user) return "Invalid email or password.";
+
+	// Set the user's ID in a signed cookie.
+	c.cookie.set("userId", user.id);
+	throw c.redirect("/profile");
 }
